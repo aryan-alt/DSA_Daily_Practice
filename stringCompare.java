65665263e82fe853bf03294d5395bb8349eab437
aryan-alt <59770426+aryan-alt@users.noreply.github.com>
2021-12-26 23:58:17 +0530

    Add files via upload

---
+++ stringCompare.java
@@ -0,0 +1,36 @@
+
+public class Main
+{
+	
+public static void StringCompare(String str){
+	    
+int len = str.length();
+	    
+String strNew = "";
+	    
+	    
+strNew += str.charAt(0);
+	   
+ for(int i = 1; i < len-1; i++){
+	        
+if((str.charAt(i-1) == 'S' || str.charAt(i-1) == 's')&(str.charAt(i+1) == 'R' || str.charAt(i+1) == 'r'))
+{continue;
+}
+else{strNew += str.charAt(i);}
+	    
+}
+	    
+	    
+strNew += str.charAt(len-1);
+	    
+System.out.println(strNew);
+	
+}
+	
+	
+public static void main(String[] args) {
+		
+StringCompare("SarkSurvey");
+}
+
+}
