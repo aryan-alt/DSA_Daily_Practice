0e0a2efc8bad13f12ce76959be8b8ec75ce3aadf
aryan-alt <59770426+aryan-alt@users.noreply.github.com>
2022-01-25 00:55:14 +0530

    Add files via upload

---
+++ containsDuplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+       // HashMap<Integer, Integer> map = new HashMap<>();
+        Set<Integer> hs = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(hs.contains(nums[i])){
+                return true;
+            }
+            hs.add(nums[i]);
+            
+        }
+        return false;
+    }
+}
+
+
