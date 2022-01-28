13710f724c632f952e4c13c17ba48d9ce0697097
Aryan Prasad <59770426+aryan-alt@users.noreply.github.com>
2022-01-28 20:10:10 +0530

    Add files via upload

---
+++ postorderTraversal.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class postorderTraversal {
+    
+      private void dfs(TreeNode node, List<Integer> postorder) {
+        if(node == null) return; 
+        
+        dfs(node.left, postorder);
+        dfs(node.right, postorder); 
+        postorder.add(node.val); 
+    }
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<Integer>(); 
+        dfs(root, ans);
+        return ans;
+    }
+}
